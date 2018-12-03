fbe9b706620478b1eb7d7fb7661da2ce15c91015
Anthony <anthonymarques@outlook.com>
2018-12-03 22:18:24 +0200

    added Routing cleared HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SettingComponent } from './setting/setting.component';
+import {WeatherService} from './weather.service';
+import {RouterModule} from '@angular/router';
+
+const appRoutes=[
+  {
+    path:'/',component: HomeComponent
+  },
+  {
+    path:'/setting', component: SettingComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    routerModule.forRoot(appRouters)
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
