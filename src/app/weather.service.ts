7643b6e32fe301e6c460386f0cea0107347e85a9
Anthony <anthonymarques@outlook.com>
2018-12-03 22:59:05 +0200

    working on Weather service with api calls and what not

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  constructor() { }
+  apiKey='53f9d8e4213222cf517d86dc406d67fc';
+  url='http://api.openweathermap.org/data/2.5/weather';
+
+  constructor(private http:Http) {
+    this.url='http://api.openweathermap.org/data/2.5/weather'
+   }
 }
