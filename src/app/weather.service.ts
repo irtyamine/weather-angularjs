234a858678d2aaa0840bafbe6aae660614bab60e
Anthony Marques <anthony@armorica.gk>
2018-12-04 08:28:22 +0200

    added find Location of user

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
-import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +8,37 @@ import 'rxjs/add/operator/map';
 export class WeatherService {
 
   apiKey='53f9d8e4213222cf517d86dc406d67fc';
-  url='http://api.openweathermap.org/data/2.5/weather';
+  url='';
 
   constructor(private http:Http) {
     this.url='http://api.openweathermap.org/data/2.5/weather'
    }
 
-   getWeather(long,lat){
-     return this.http.get(url: this.url+'?lat='+lat+'&lon='+long+'&appid='+this.apiKey)
+   getWeather(lng,lat){
+     return this.http.get(this.url+'?lat='+lat+'&lon='+lng+'&appid='+this.apiKey)
    }
+
+   geoFindMe() {
+    // var output = document.getElementById("out");
+  
+    if (!navigator.geolocation){
+      console.log("<p>Geolocation is not supported by your browser</p>");
+      return;
+    }
+  
+    function success(position) {
+      var latitude  = position.coords.latitude;
+      var longitude = position.coords.longitude;
+  
+      console.log('<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>');
+    }
+  
+    function error() {
+      console.log("Unable to retrieve your location");
+    }
+  
+    console.log("<p>Locating…</p>");
+  
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
 }
