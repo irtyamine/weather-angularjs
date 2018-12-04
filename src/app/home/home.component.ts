5eaca35d7f4d90079a61091865d58e5f19b64c6a
Anthony Marques <anthony@armorica.gk>
2018-12-04 11:10:00 +0200

    updated response to be displayed in JSON formate added rxjs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import {WeatherService} from '../weather.service';
+import { Component, OnInit } from "@angular/core";
+import { WeatherService } from "../weather.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  location = {
+    Latitude: -33.886734,
+    Longitude: 18.5058874
+  };
 
-  location={
-    Latitude:-33.886734,
-    Longitude:18.5058874
-  }
-
-  constructor(private _weatherService:WeatherService) { }
+  constructor(private _weatherService: WeatherService) {}
 
   ngOnInit() {
     this._weatherService.geoFindMe();
-    this._weatherService.getWeather(this.location.Longitude,this.location.Latitude).subscribe((response)=>{
-      console.log(response);
-    })
+    this._weatherService
+      .getWeather(this.location.Longitude, this.location.Latitude)
+      .subscribe(response => {
+        console.log(response);
+      });
   }
 
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
-import { filter } from 'rxjs/operators';
+import { map } from "rxjs/operators";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable(
+//   {
+//   providedIn: 'root'
+// }
+)
 export class WeatherService {
 
   apiKey='53f9d8e4213222cf517d86dc406d67fc';
-  url='';
+  url;
+
 
   constructor(private http:Http) {
     this.url='http://api.openweathermap.org/data/2.5/weather'
    }
 
    getWeather(lng,lat){
-     return this.http.get(this.url+'?lat='+lat+'&lon='+lng+'&appid='+this.apiKey);
+     return this.http.get(this.url+'?lat='+lat+'&lon='+lng+'&appid='+this.apiKey).pipe(map(res => res.json()));
     // console.log("toasty",lng,' ', lat);
    }
 
