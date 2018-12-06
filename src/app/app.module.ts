9e5e31488101a15cd10391c292d2c4dd5182a7b7
Anthony Marques <anthony@armorica.gk>
2018-12-06 18:12:37 +0200

    restructuring and fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WeatherService } from './services/weather.service';
-import {HttpModule} from '@angular/http';
+import { HttpModule } from '@angular/http';
+import { ForecastComponent } from './forecast/forecast.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,
