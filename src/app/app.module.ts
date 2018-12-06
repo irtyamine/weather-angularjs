623e0240ea75b9f54b677f7238040b6ef3d8db7e
Anthony Marques <anthony@armorica.gk>
2018-12-06 17:59:55 +0200

    updated namings and removed unneccessary files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { CurrentComponent } from './current/current.component';
 import { WeatherService } from './weather.service';
-import { MatProgressSpinnerModule } from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { LoaderService } from './loader.service';
 import {HttpModule} from '@angular/http';
 
@@ -15,14 +12,11 @@ import {HttpModule} from '@angular/http';
   declarations: [
     AppComponent,
     HeaderComponent,
-    CurrentComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     AppRoutingModule,
-    BrowserAnimationsModule
-    // MatProgressSpinnerModule
   ],
   providers: [WeatherService,
     LoaderService],
