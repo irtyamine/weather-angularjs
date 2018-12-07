1f28861c4e6ed0c3e9a65ee8fe0ea44cdb36a08b
Anthony Marques <anthony@armorica.gk>
2018-12-07 07:25:07 +0200

    fixed up directives to return degrees

--- src/app/directives/celcius.directive.ts
+++ src/app/directives/celcius.directive.ts
@@ -5,8 +5,8 @@ export class ToCelcius implements PipeTransform {
 
   transform(fh: string ): string {
       console.log(fh);
-    var cel = Math.round(parseInt(fh, 10) - 273.15);;
-    return `${cel}`;
+    var cel = Math.round(parseInt(fh, 10) - 273.15);
+    return `${cel}°`;
   }
 
 }
