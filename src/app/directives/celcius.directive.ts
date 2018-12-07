53b86898e5388a5a9d9c916a43b8830803204b0d
Anthony Marques <anthony@armorica.gk>
2018-12-07 12:56:51 +0200

    fixing up styling and syntax

--- src/app/directives/celcius.directive.ts
+++ src/app/directives/celcius.directive.ts
@@ -1,8 +1,8 @@
-import { Pipe, PipeTransform } from "@angular/core";
+import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({ name: "toCelcuis" })
+@Pipe({ name: 'toCelcuis' })
 export class ToCelcius implements PipeTransform {
-    
+
   transform(fh: string): string {
     var cel = Math.round(parseInt(fh, 10) - 273.15);
     return `${cel}°`;
